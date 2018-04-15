2a1db349d7e10b1b88a94017e20012e801295edf
Falco Gerritsjans <falcog@users.noreply.github.com>
2018-04-15 21:23:44 +0200

    feat: Create player messages and player commands

--- examples/helloworld/index.js
+++ examples/helloworld/index.js
@@ -2,14 +2,26 @@ const MultiPlayer = require('../..');
 
 class GameMode extends MultiPlayer {
   constructor () {
-    super();
-
-    console.log(this);
+    super(1338);
   }
 }
 
 const Hello = new GameMode;
 
-Hello.server.on('init', () => {
-  console.log('init?!')
+Hello.player.on('connect', e => {
+  console.log('Player connected!');
+});
+
+Hello.player.on('text', e => {
+  if (e.message.indexOf('blacklisted') === -1) {
+    e.promise.resolve(`Team Blue ${e.message}`);
+  } else {
+    e.promise.reject('Forbidden')
+  }
+});
+
+Hello.player.on('command', e => {
+  console.log('Command:', e);
+
+  e.promise.reject('Does this actually work?');
 });
